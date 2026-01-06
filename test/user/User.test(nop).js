382b3292edac7e92e1bc80f5981bddfc5994bf6e
Grondin Florent <florent.grondin97@gmail.com>
2026-01-06 12:05:01 +0100

    add first ci test

---
+++ test/user/User.test(nop).js
@@ -0,0 +1,23 @@
+// const User = require('../../User/User');
+
+// test('user valide', () => {
+//     const user = new User('Florent', 'ESGI', 24, 'florent.esgi@example.com', '15/06/2000');
+//     expect(user.isValid()).toBe(true);
+// });
+// test('user avec email invalide', () => {
+//     const user = new User
+//     ('Test', 'testnom', 30, 'test@test', '22/10/2025');
+//     expect(user.isValid()).toBe(false);
+// });
+// test('user de moins de 13 ans', () => {
+//     const user = new User('Kid', 'User', 10, 'kid.user@example.com', '01/01/2020');
+//     expect(user.isValid()).toBe(false);
+// });
+// test('user sans nom de famille', () => {
+//     const user = new User('NoLastName', '', 20, 'nolastname@example.com', '10/05/2003');
+//     expect(user.isValid()).toBe(false);
+// });
+// test('user sans prénom', () => {
+//     const user = new User('', 'NoFirstName', 22, 'nofirstname@example.com', '15/08/2001');
+//     expect(user.isValid()).toBe(false);
+// });
